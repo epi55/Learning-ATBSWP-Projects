a5a59b2032d2bf0cb3422a6ed3670df0c314b099
epi55 <87436238+epi55@users.noreply.github.com>
2022-05-31 09:26:24 -0700

    Update ATBSWP_C3P1.py

--- ATBSWP_C3P1.py
+++ ATBSWP_C3P1.py
@@ -5,14 +5,14 @@
 def collatz(number):
     if number % 2 == 0: # Determines if number is even, using modulo
         print("The number you entered, " + str(number) + ", is even!")
-        print(number / 2) # UNEXPETED: A float is printed. Why?
+        print(number / 2) # UNEXPECTED: A float is printed. Why?
     elif number % 2 == 1: # Determines if number is odd, using modulo
         print("The number you entered, " + str(number) + ", is odd!")
         print(3 * number + 1)
     else:
         print("I'm uncertain what happened...") # Should never be printed, why was it? 
 
-# Requesting input for function
+# Input request, running function, and ValueError exception
 print("What is your number?")
 try:
     number = int(input())
